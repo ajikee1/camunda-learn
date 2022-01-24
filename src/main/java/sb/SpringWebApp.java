8627d33b5008250aa83dec056f7082afa8adc69d
ajikee1 <akeeri1@students.towson.edu>
2022-01-24 11:24:56 -0500

    init commit

---
+++ src/main/java/sb/SpringWebApp.java
@@ -0,0 +1,17 @@
+package sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/* https://docs.camunda.org/get-started/spring-boot/project-setup/ */
+
+@SpringBootApplication
+public class SpringWebApp {
+
+    public static void main(String[] args) {
+
+            SpringApplication.run(SpringWebApp.class, args);
+
+    }
+}
